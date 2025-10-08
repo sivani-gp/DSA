70eb293daac6379706e958afe85a21106412aead
Sivani Gandupilli <146966803+sivani-gp@users.noreply.github.com>
2025-10-08 18:51:12 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (45.22%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = l + ((r - l) // 2)
+
+            if ((m - 1 < 0 or nums[m - 1] != nums[m]) and
+                (m + 1 == len(nums) or nums[m] != nums[m + 1])):
+                return nums[m]
+
+            leftSize = m - 1 if nums[m - 1] == nums[m] else m
+
+            if leftSize % 2 == 0:
+                l = m + 1
+            else:
+                r = m - 1
+
+        
